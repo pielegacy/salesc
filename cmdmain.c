2a16a2178bd9da4349daf7b84812fb4b7b6cbb83
pielegacy <alex_billson@outlook.com>
2016-04-19 12:55:52 +1000

    Stripped down cmdmain for UI development

--- cmdmain.c
+++ cmdmain.c
@@ -15,15 +15,10 @@ int main(int argc, char *argv[]){
     Sell *testsale = new_sell(30, testproduct, testpaytype, 100.0);
     printf("Sales.c Command Line Client\n");
     printf("Development Build 0.2\n");
-    // printf("TEST PRODUCT ID : %d\n", find_product_id("Not A Computer Science Student"));
     db_create();
-    // test_value();
-    //add_product(test);
-    add_payment(testpay);
-    add_sell(testsale);
-    // last_sale();
-    console_readproductnames();
-    // printf("The version of Sqlite3 that is being ran is version %s\n", sqlite3_libversion());
+    //add_payment(testpay);
+    //add_sell(testsale);
+    //console_readproductnames();
     getchar();
     return 0;
 }
