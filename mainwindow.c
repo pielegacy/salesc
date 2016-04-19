a3d3f7ea09025077b90ea09fe2ef70732f4c2a7b
pielegacy <strikingmongoose@hotmail.com>
2016-04-19 15:42:26 +1000

    Added UI Base

--- mainwindow.c
+++ mainwindow.c
@@ -6,12 +6,28 @@
 #include <sqlite3.h>
 #include <unistd.h>
 
+static void change_text(GtkWidget *widget, GtkEntry *data){
+    const char* output = gtk_entry_get_text(data);
+    printf("The text is : %s\n", output);
+}
+
 int main(int argc, char *argv[]){
     GtkBuilder *builder;
     gtk_init(&argc, &argv);
-    
+    GObject *window;
+    GtkEntry *input;
+        
     builder = gtk_builder_new();
     gtk_builder_add_from_file(builder, "ui/main.ui", NULL);
+    
+    window = gtk_builder_get_object(builder, "mainwindow");
+    g_signal_connect (window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    input = GTK_ENTRY(gtk_builder_get_object(builder, "test_entry"));
+    // g_signal_connect(input, "changed", G_CALLBACK(change_text), &input);
+    
+    
+    
     gtk_main();
+    
     return 0;
 }
